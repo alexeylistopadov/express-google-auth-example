14bf0ef640210d265eb733fa9b9efe0c439569a9
OleksiiLy <alexey.listopadov@gmail.com>
2018-07-15 15:31:55 +0300

    Add simple google auth

--- server.js
+++ server.js
@@ -1,10 +1,52 @@
-var express = require('express');
-var app = express();
+const express = require('express'),
+    app = express(),
+    passport = require('passport'),
+    auth = require('./auth'),
+    cookieParser = require('cookie-parser'),
+    cookieSession = require('cookie-session'),
+    path = require('path');
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.set('view engine', 'pug')
+
+auth(passport);
+app.use(passport.initialize());
+
+app.use(cookieSession({
+    name: 'session',
+    keys: ['SECRECT KEY']
+}));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+    if (req.session.token) {
+        res.cookie('token', req.session.token);
+        res.send('<a href="/logout">Logout</a>');
+    } else {
+        res.cookie('token', '')
+        res.send('<a href="/auth/google">Login with google</a>');
+    }
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/logout', (req, res) => {
+    req.logout();
+    req.session = null;
+    res.redirect('/');
+});
+
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['https://www.googleapis.com/auth/userinfo.profile']
+}));
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', {
+        failureRedirect: '/'
+    }),
+    (req, res) => {
+        req.session.token = req.user.token;
+        res.redirect('/');
+    }
+);
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
 });
